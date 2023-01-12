816190825b7589fa6a3a78813c6e45e9df3d3526
xangjong <csj7080@naver.com>
2023-01-12 21:14:14 +0900

    Update TimeTraceAop

--- hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -1,2 +1,33 @@
-package hello.hellospring.aop;public class TimeTraceAop {
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import javax.xml.transform.Result;
+
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..)) ")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+
+
+    }
+
+
 }
