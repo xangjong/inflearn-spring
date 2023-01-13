3a6850aa5b5e9beeaa91131674e9a54d477e6461
xangjong <csj7080@naver.com>
2023-01-13 21:57:37 +0900

    Update HomeController

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.hellospring.controller;
+package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
