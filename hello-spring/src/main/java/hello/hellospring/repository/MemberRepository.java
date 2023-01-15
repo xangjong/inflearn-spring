06f0f3021c3c8458b7583ca3b6dcb4b21c008b04
xangjong <csj7080@naver.com>
2023-01-15 17:46:05 +0900

    Update MemberRepository

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,7 +1,6 @@
-package com.example.hellospring.repository;
+package hello.hellospring.repository;
 
-import com.example.hellospring.domain.Member;
-import org.springframework.stereotype.Repository;
+import hello.hellospring.domain.Member;
 
 import java.util.List;
 import java.util.Optional;
